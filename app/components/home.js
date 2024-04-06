24e9529e21795f503af74835bff6f6b417f39361
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-04-06 15:48:50 +0530

    ....

--- app/components/home.js
+++ app/components/home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <title>WTT - Home</title>
+      <div>Home</div>
+    </>
+  );
 }
 
 export default Home;
