56080e4619969658318384048d7311e4d710dac6
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-04-07 17:13:04 +0530

    ui changes pt2

--- app/components/loadingScreen.js
+++ app/components/loadingScreen.js
@@ -5,8 +5,8 @@ import Box from "@mui/material/Box";
 function LoadingScreen() {
   return (
     <div className=" flex justify-center items-center h-[100vh]">
-      <Box sx={{display: "flex"}}>
-        <CircularProgress size={50} color="success" />
+      <Box sx={{display: "flex", color: "#65a30d"}}>
+        <CircularProgress size={70} color="inherit" />
       </Box>
     </div>
   );
