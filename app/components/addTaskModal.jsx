0a172ff678bdcfa512ebd28d96f8ff32f42f38d8
Aaditya Paul <aadityapaul2006.programmer@gmail.com>
2024-09-10 20:49:19 +0530

    add task ui added

--- app/components/addTaskModal.jsx
+++ app/components/addTaskModal.jsx
@@ -4,7 +4,14 @@ import Close from "../../public/assets/icons/study.png";
 import Image from "next/image";
 import ViewMembers from "./viewMembers";
 import {usePathname} from "next/navigation";
-import {collection, getDocs, query, where} from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import {db} from "@/firebase";
 import LoadingComponent from "./loadingComponent";
 function AddTaskModal({setToggleModal, toggleModal}) {
@@ -14,7 +21,8 @@ function AddTaskModal({setToggleModal, toggleModal}) {
   const [project, setProject] = useState(null);
   const [projectName, setProjectName] = useState("");
   const [loading, setLoading] = useState(true);
-
+  const [projectMember, setProjectMember] = useState(null);
+  const [memberDetails, setMemberDetails] = useState(null);
   useEffect(() => {
     const getProjectData = async () => {
       try {
@@ -26,16 +34,43 @@ function AddTaskModal({setToggleModal, toggleModal}) {
         querySnapshot.forEach((doc) => {
           const data = doc.data();
           setProject(data);
-          console.log(data);
+          //   console.log(data.members);
         });
       } catch (error) {
         console.error("Error getting project data:", error);
-      } finally {
-        setLoading(false);
       }
     };
+
     getProjectData();
   }, []);
+
+  useEffect(() => {
+    const getMembers = async () => {
+      var Member = [];
+      for (let i = 0; i < project?.members.length; i++) {
+        const docRef = await getDoc(doc(db, "users", project.members[i]));
+        if (docRef.exists()) {
+          //   console.log(docRef.data());
+          try {
+            Member.push({
+              name: docRef.data().displayName,
+              pfp: docRef.data().photoURL,
+              uid: docRef.data().uid,
+            });
+          } catch (e) {
+            console.log(e);
+          } finally {
+            setLoading(false);
+          }
+        } else {
+          console.log("No such document!");
+        }
+      }
+      setMemberDetails(Member);
+    };
+    getMembers();
+    setProjectMember(project?.members);
+  }, [project]);
 
   useEffect(() => {
     const handleEscapeKeyPress = (event) => {
@@ -51,8 +86,8 @@ function AddTaskModal({setToggleModal, toggleModal}) {
     };
   }, [setToggleModal, toggleModal]);
   return (
-    <div className=" flex h-[100vh] w-[100vw] justify-center items-center overflow-hidden ">
-      <div className=" z-40 w-fit h-[80%] bg-white rounded-lg shadow-2xl p-4 overflow-y-auto  ">
+    <div className="  flex h-[100vh] w-[100vw] justify-center items-center overflow-hidden ">
+      <div className=" z-40 w-fit h-[80%] bg-white rounded-lg shadow-2xl p-4 overflow-y-auto no-scrollbar  ">
         <div className=" flex justify-between align-middle">
           {/* heading */}
           <div className=" font-doasis font-semibold text-3xl">Add Task</div>
@@ -94,13 +129,41 @@ function AddTaskModal({setToggleModal, toggleModal}) {
             </div>
             {/* <input className=" p-2 px-0 outline-none border-b border-lime-700" /> */}
             <div>
-              {loading ? (
-                <LoadingComponent />
-              ) : (
-                <div className=" flex justify-between  items-center">
-                  <ViewMembers project={project} members={project?.members} />
-                  hi
-                </div>
+              {!toggleModal ? null : (
+                <>
+                  <div className=" flex flex-col no-scrollbar justify-start  h-32  overflow-y-scroll">
+                    {memberDetails.map((e, index) => {
+                      return (
+                        <>
+                          <div className=" flex items-center justify-between">
+                            <div className=" flex gap-3 items-center my-1">
+                              <div className=" relative w-8 h-8 ">
+                                <Image
+                                  className=" rounded-full"
+                                  fill
+                                  src={e.pfp}
+                                />
+                              </div>
+                              <div key={index}>{e.name}</div>
+                            </div>
+                            <input
+                              name="memcheck"
+                              className=" w-5 h-5 accent-lime-600 "
+                              type="checkbox"
+                              onChange={(e) => {
+                                console.log(e.target.checked);
+                              }}
+                            />
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>
+
+                  <div className=" my-2 flex justify-center items-center p-4 border border-lime-700 hover:bg-lime-700 hover:text-white text-xl cursor-pointer transition-all ease-linear">
+                    <div>Add Task</div>
+                  </div>
+                </>
               )}
             </div>
           </div>
